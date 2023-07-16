4cd235e4558c6e6fdfb6847686f5bbf2fc38e27b
Baris Mutlu <baris@mutlu.dev>
2023-07-16 19:00:23 +0300

    Check if incoming userId is valid

--- backend/src/routes/middleware.js
+++ backend/src/routes/middleware.js
@@ -1,6 +1,11 @@
 const User = require('../models/user')
+const mongoose = require('mongoose')
 
 async function setUser(req, res, next) {
+  if(!mongoose.Types.ObjectId.isValid(req.query.userId)){
+    req.user = null
+    return next()
+  }
   req.user = await User.findById(req.query.userId)
   next()
 }
