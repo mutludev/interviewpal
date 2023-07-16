cc4b7f7e525f1a7e8c3e0960fff40964e9316ecf
Baris Mutlu <baris@mutlu.dev>
2023-07-16 19:01:02 +0300

    Add document methods to user model

--- backend/src/models/user.js
+++ backend/src/models/user.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose')
 const jobSchema = require("./jobSchema");
+const Document = require('./document')
 
 const user = new mongoose.Schema({
   name: String,
@@ -18,5 +19,21 @@ const user = new mongoose.Schema({
     ref: 'Document'
   }],
 })
+
+user.methods.getDocuments = async function() {
+  let documents = []
+  for(let documentId of this.documents) {
+    let document = await Document.findById(documentId)
+    documents.push(document)
+  }
+  return documents
+}
+
+user.methods.getDocument = async function(id) {
+  if(!this.documents.includes(id)) throw new Error('Document not found')
+  let document = await Document.findById(id)
+  if(!document) throw new Error('Document not found')
+  return document
+}
 
 module.exports = mongoose.model('User', user)
