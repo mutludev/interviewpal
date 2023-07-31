aedd3181b434fb5432602c630dea44b217133487
Baris Mutlu <baris@mutlu.dev>
2023-08-01 00:58:50 +0300

    Early return if statements

--- backend/src/routes/middleware.js
+++ backend/src/routes/middleware.js
@@ -2,20 +2,18 @@ const User = require('../models/user')
 const mongoose = require('mongoose')
 
 async function setUser(req, res, next) {
-  if(!mongoose.Types.ObjectId.isValid(req.query.userId)){
-    req.user = null
-    return next()
+  req.user = null
+  if(mongoose.Types.ObjectId.isValid(req.query.userId)){
+    req.user = await User.findById(req.query.userId)
   }
-  req.user = await User.findById(req.query.userId)
   next()
 }
 
 async function ensureSession(req, res, next) {
-  if (req.user) {
-    next()
-  } else {
-    res.status(401).send('You must be logged in to do that.')
+  if (!req.user) {
+    return res.status(401).send('You must be logged in to do that.')
   }
+  next()
 }
 
 module.exports = {
