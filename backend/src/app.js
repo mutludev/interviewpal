483d7d7f22b38ef7a08d082887898843e015a4c9
Baris Mutlu <baris@mutlu.dev>
2023-08-01 00:59:45 +0300

    Organize structure

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,18 +1,17 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 const fileUpload = require('express-fileupload');
-
-require('./db-connection')
+const { setUser } = require('./routes/middleware')
 
 const app = express()
 app.use(bodyParser.json())
 app.use(fileUpload());
-const port = 3000
-const api = require('./routes/api')
+app.use(setUser)
 
-app.use('/api', api)
+require('./db-connection')
 
+app.use('/api', require('./routes/api'))
 
-app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`)
+app.listen(3000, () => {
+    console.log(`Example app listening on port ${3000}`)
 })
--- backend/src/routes/api.js
+++ backend/src/routes/api.js
@@ -1,17 +1,8 @@
 const express = require('express')
 const router = express.Router()
 
-const middleware = require('./middleware')
-
-const job = require('./job')
-const user = require('./user')
-const document = require('./document')
-
-
-router.use(middleware.setUser)
-
-router.use('/job', job)
-router.use('/user', user)
-router.use('/document', document)
+router.use('/job', require('./job'))
+router.use('/user', require('./user'))
+router.use('/document', require('./document'))
 
 module.exports = router
