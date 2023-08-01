adec3126b183e69916a4d0bb62598fac7bdbedf3
Baris Mutlu <baris@mutlu.dev>
2023-08-01 18:11:10 +0300

    Create routes

--- frontend/src/router/index.ts
+++ frontend/src/router/index.ts
@@ -1,8 +1,30 @@
 import { createRouter, createWebHistory } from 'vue-router'
+// @ts-ignore
+import Main from '@/layouts/Main.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: []
+  routes: [
+    { path: '/',
+      component: Main,
+      children: [
+        {
+          path: '',
+          name: 'Interviews',
+          // @ts-ignore
+          component: () => import('@/pages/Interviews.vue'),
+          meta: { title: 'Interviews' }
+        },
+        {
+          path: 'documents',
+          name: 'Documents',
+          // @ts-ignore
+          component: () => import('@/pages/Documents.vue'),
+          meta: { title: 'Documents' }
+        }
+      ]
+    },
+  ]
 })
 
 export default router
