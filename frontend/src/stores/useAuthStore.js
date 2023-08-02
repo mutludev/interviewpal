ad12bde34118ed71208ac763faab87080b86f107
Baris Mutlu <baris@mutlu.dev>
2023-08-02 22:52:49 +0300

    Create useAuthStore.js

---
+++ frontend/src/stores/useAuthStore.js
@@ -0,0 +1,61 @@
+import { defineStore } from 'pinia'
+import axios from 'axios'
+axios.defaults.withCredentials = true
+axios.defaults.baseURL = 'http://localhost:3000/api/user'
+
+export const useAuthStore = defineStore('auth', {
+  state: () => ({
+    user: null,
+    loading: false
+  }),
+  actions: {
+    async login(email, password) {
+      this.loading = true
+      try{
+        const res = await axios.post('/login', {
+          email,
+          password
+        })
+        this.user = res.data.user
+        return this.user
+      } catch(err) {
+        console.log(err)
+      } finally {
+        this.loading = false
+      }
+    },
+    logout() {},
+    async register(username, email, password) {
+      this.loading = true
+      try {
+        const res = await axios.post('/', {
+          username,
+          email,
+          password
+        })
+        this.user = res.data.user
+        return this.user
+      } catch(err) {
+        console.log(err)
+      } finally {
+        this.loading = false
+      }
+    },
+    async getUser() {
+      this.loading = true
+      try {
+        const res = await axios.get('/')
+        this.user = res.data
+        return this.user
+      } catch(err) {
+        console.log(err)
+      } finally {
+        this.loading = false
+      }
+    }
+  },
+  getters: {
+    isLoggedIn: (state) => state.user,
+    isLoading: (state) => state.loading
+  }
+})
