2f11203ad15896698b8c58f186de99518086e55d
Baris Mutlu <baris@mutlu.dev>
2023-08-02 22:37:39 +0300

    Auth modal visibility
    Show Auth modal If user is not logged in and not on page load

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,8 +1,15 @@
 <script setup>
 import LoginModal from '@/components/AuthModalWrapper.vue'
+import { ref } from 'vue'
+import { useAuthStore } from '@/stores/useAuthStore'
+const authStore = useAuthStore()
+const isCheckedAuth = ref(false)
+authStore.getUser().finally(() => {
+  isCheckedAuth.value = true
+})
 </script>
 
 <template>
   <router-view/>
-  <login-modal/>
+  <login-modal v-if='!authStore.isLoggedIn && isCheckedAuth.value'/>
 </template>
