3ce8f87f707e3b209d939375d94edd1405994e9a
Baris Mutlu <baris@mutlu.dev>
2023-08-02 16:25:41 +0300

    Add Auth modal

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,3 +1,8 @@
+<script setup>
+import LoginModal from '@/components/AuthModalWrapper.vue'
+</script>
+
 <template>
   <router-view/>
+  <login-modal/>
 </template>
---
+++ frontend/src/components/AuthModal.vue
@@ -0,0 +1,50 @@
+<script setup>
+import LoginForm from '@/components/LoginForm.vue'
+import RegisterForm from '@/components/RegisterForm.vue'
+import { ref } from 'vue'
+
+const isLogin = ref(true)
+</script>
+
+<template>
+  <div class='modal'>
+    <div class='greeting'>Welcome the Intervio</div>
+    <login-form v-if="isLogin" />
+    <register-form v-else />
+
+    <button class='auth-switcher' @click="isLogin = !isLogin">
+      {{ isLogin ? "Don't have an account" : 'Have an account' }}
+    </button>
+  </div>
+
+</template>
+
+<style scoped>
+.modal {
+  background-color: #171717;
+  min-width: 350px;
+  min-height: 400px;
+  border-radius: 10px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: space-between;
+  padding: 20px;
+}
+
+.auth-switcher {
+  background-color: transparent;
+  border: none;
+  color: #ebebeb;
+  font-size: 14px;
+  margin-top: 10px;
+  cursor: pointer;
+  text-decoration: underline;
+}
+.greeting {
+  color: #ebebeb;
+  font-size: 20px;
+  font-weight: 600;
+    margin-bottom: 20px;
+}
+</style>
