7994d3a11883c972543c4dd87b96900297730f0e
Baris Mutlu <baris@mutlu.dev>
2023-08-02 23:49:49 +0300

    Configure ant design

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -5,11 +5,15 @@ import { createPinia } from 'pinia'
 import router from './router'
 import '@/assets/base.css'
 import 'primeicons/primeicons.css'
+import 'ant-design-vue/dist/reset.css';
+import Antd from 'ant-design-vue';
+
 
 const pinia = createPinia()
 const app = createApp(App)
 
 app.use(router)
 app.use(pinia)
+app.use(Antd)
 
 app.mount('#app')
