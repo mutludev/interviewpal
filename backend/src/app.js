a562c1a6db8ccedaa24fd4472249217dca7d2013
Baris Mutlu <baris@mutlu.dev>
2023-08-02 20:08:18 +0300

    Configure client-sessions library

--- backend/src/app.js
+++ backend/src/app.js
@@ -2,11 +2,23 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const fileUpload = require('express-fileupload');
 const { setUser } = require('./routes/middleware')
+const sessions = require("client-sessions");
+const dotenv = require('dotenv')
+dotenv.config()
+
 
 const app = express()
+app.use(sessions({
+    cookieName: 'authSession', // cookie name dictates the key name added to the request object
+    secret: process.env.SECRET, // should be a large unguessable string
+    duration: 24 * 60 * 60 * 1000, // how long the session will stay valid in ms
+    activeDuration: 1000 * 60 * 5 // if expiresIn < activeDuration, the session will be extended by activeDuration milliseconds
+}));
 app.use(bodyParser.json())
 app.use(fileUpload());
 app.use(setUser)
+
+
 
 require('./db-connection')
 
