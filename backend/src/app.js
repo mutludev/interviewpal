854a49815568e29f000d24224ea0fbddd4107883
Baris Mutlu <baris@mutlu.dev>
2023-08-12 21:32:08 +0300

    Make port dynamic

--- backend/src/app.js
+++ backend/src/app.js
@@ -29,6 +29,7 @@ require('./db-connection')
 
 app.use('/api', require('./routes/api'))
 
-app.listen(3000, () => {
+const port = process.env.PORT || 3000
+app.listen(port, () => {
     console.log(`Example app listening on port ${3000}`)
 })
