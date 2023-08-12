60dec9eb5a30a9c83f6babf82e925595a06b8f2a
Baris Mutlu <baris@mutlu.dev>
2023-08-12 21:46:24 +0300

    Update origin host

--- backend/src/app.js
+++ backend/src/app.js
@@ -9,7 +9,7 @@ dotenv.config()
 
 const app = express()
 app.use((req, res, next) => {
-    res.header("Access-Control-Allow-Origin", "interviewpal.onrender.com"); // update to match the domain you will make the request from
+    res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
 });
