2d37bf7172bbf811e4ce44c359164267385e6db5
Baris Mutlu <baris@mutlu.dev>
2023-08-19 00:40:14 +0300

    Ad loading state to interview store

--- frontend/src/stores/useInterviewStore.js
+++ frontend/src/stores/useInterviewStore.js
@@ -3,18 +3,23 @@ import axios from 'axios'
 export const useInterviewStore = defineStore('interview',{
     state: () => ({
       interviews: [],
+      isLoading: false,
     }),
     actions: {
       async fetchInterviews() {
+        this.isLoading = true;
         try {
           const response = await axios.get('/api/job/');
           this.interviews = response.data;
         } catch (error) {
           console.log(error);
+        } finally {
+          this.isLoading = false;
         }
       }
     },
     getters: {
-      getInterviews: (state) => state.interviews
+      getInterviews: (state) => state.interviews,
+      getLoading: (state) => state.isLoading
     }
 })
