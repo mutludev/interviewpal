6e67a15ab600e4bb0bb295d8502caba15f0a4639
Barış Mutlu <baris@mutlu.dev>
2023-12-06 15:02:49 +0300

    Add GenericTable component

---
+++ frontend/src/components/GenericTable.vue
@@ -0,0 +1,37 @@
+<script setup>
+  defineProps(['headers', 'items'])
+</script>
+
+<template>
+  <table>
+    <tr>
+      <th v-for="header in headers" :key="header.value">
+        {{ header.text }}
+      </th>
+    </tr>
+    <tr v-for="item in items" :key="item.id">
+      <td v-for="header in headers" :key="header.value">
+        {{ item[header.value] }}
+      </td>
+    </tr>
+  </table>
+</template>
+
+<style scoped>  
+table {
+  width: 100%;
+  text-align: left;
+  margin: 10px 10px;
+}
+
+th {
+  font-weight: bold;
+  padding: 10px 0px;
+  border-bottom: 1px solid #ddd;
+}
+
+td {
+  padding: 10px 0px;
+  border-bottom: 1px solid #787878;
+}
+</style>
