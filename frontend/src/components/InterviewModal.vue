70ad36f6d263b0446be3bdbbaa4fd7e9091ff799
Barış Mutlu <baris@mutlu.dev>
2024-01-27 00:11:00 +0300

    Refactoring InterviewModal and Interviews

--- frontend/src/components/InterviewModal.vue
+++ frontend/src/components/InterviewModal.vue
@@ -1,18 +1,28 @@
 <script setup>
+  import ModalWrapper from './ModalWrapper.vue';
+  import { ref, defineEmits } from 'vue'
+  
   import { useInterviewStore } from '@/stores/useInterviewStore';
   import { useInterviewModalStore } from '@/stores/useInterviewModalStore';
-  import { ref } from 'vue'
-  const interviewStore = useInterviewStore()
   const interviewModalStore = useInterviewModalStore()
+  const interviewStore = useInterviewStore()
+
+  const emit = defineEmits(['close'])
+
+  const data = ref(interviewModalStore.content ? interviewModalStore.content : {})  
+
   const activeKey = ref('1')
-  const title = ref('')
-  const company = ref('')
 
   async function save() {
-    await interviewStore.addInterview({
-      title: title.value,
-      company: company.value
-    })
+    console.log(data.value)
+    if (!data.value.hasOwnProperty('_id')) {
+      await interviewStore.addInterview(data.value)
+    } else {
+      await interviewStore.updateInterview({
+        id: data._id,
+        ...data.value
+      })
+    }
     interviewModalStore.closeModal()
   }
 </script>
@@ -22,19 +32,21 @@
   <modal-wrapper>
     <div class="interview-modal">
       <div class="header">
-        <a-input class="company" v-model:value="company" placeholder="Company" />
-        <a-input class="title" v-model:value="title" placeholder="Job Title" />
+        <a-input class="company" v-model:value="data.company" placeholder="Company" />
+        <a-input class="title" v-model:value="data.title" placeholder="Job Title" />
       </div>
       <a-tabs class="tab-pane" v-model:activeKey="activeKey">
         <a-tab-pane key="1" tab="General">General</a-tab-pane>
-        <a-tab-pane key="2" tab="Notes">Notes</a-tab-pane>
+        <a-tab-pane key="2" tab="Notes">
+          <a-textarea rows=4 v-model:value="data.notes" />
+        </a-tab-pane>
         <a-tab-pane key="3" tab="Contacts">Contacts</a-tab-pane>
         <a-tab-pane key="4" tab="Documents">Documents</a-tab-pane>
         <a-tab-pane key="5" tab="Actions">Actions</a-tab-pane>
 
       </a-tabs>
       <div class="footer">
-        <a-button @click="interviewModalStore.closeModal">Cancel</a-button>
+        <a-button @click="emit('close')">Cancel</a-button>
         <a-button type="primary" @click="save">Save</a-button>
       </div>
     </div>
--- frontend/src/pages/Interviews.vue
+++ frontend/src/pages/Interviews.vue
@@ -1,56 +1,58 @@
 <script setup>
 import { onMounted } from 'vue'
-import { useInterviewStore } from '@/stores/useInterviewStore'
-import { useInterviewModalStore } from '@/stores/useInterviewModalStore';
+import InterviewModal from '@/components/InterviewModal.vue';
 import GenericTable from '@/components/GenericTable.vue';
+
+import { useInterviewStore } from '@/stores/useInterviewStore'
+import { useInterviewModalStore } from '@/stores/useInterviewModalStore'
 const interviewStore = useInterviewStore()
 const interviewModalStore = useInterviewModalStore()
 
-function getURLHost(url){
-  try{
-    const urlObject = new URL(url)
-    return urlObject.host
-  } catch {
-    return url
-  }
-}
-
-function createNewJob(){
-  interviewModalStore.openModal()
-}
-
 const headers = [
-  { text: "Company", value: "company" },
-  { text: "Title", value: "title" },
-  { text: "Url", value: "url" },
-  { text: "Salary", value: "salary" },
-  { text: "Location", value: "location" },
-  { text: "Deadline", value: "deadline" },
-  { text: "Latest Action", value: "latest-action" }
+    { text: "Company", value: "company" },
+    { text: "Title", value: "title" },
+    { text: "Url", value: "url" },
+    { text: "Salary", value: "salary" },
+    { text: "Location", value: "location" },
+    { text: "Deadline", value: "deadline" },
+    { text: "Latest Action", value: "latest-action" },
+    { text: "Actions", value: "actions" }
 ];
+
 onMounted(() => {
-  interviewStore.fetchInterviews()
+    interviewStore.fetchInterviews()
 })
 </script>
 
 <template>
-  <a href='#' class="action-btn" @click.prevent='createNewJob'>
-    <span class="pi pi-plus" />
-     <span>Create new job</span>
-  </a>
-  <a href='#' class="action-btn" @click.prevent='interviewStore.fetchInterviews'>
-    <span :class="`pi pi-sync ${interviewStore.isLoading ? 'pi-spin' : ''}`" />
-    <span>Refresh</span>
-  </a>
+    <a href='#' class="action-btn" @click.prevent='interviewModalStore.openModal()'>
+        <span class="pi pi-plus" />
+        <span>Create new job</span>
+    </a>
+    <a href='#' class="action-btn" @click.prevent='interviewStore.fetchInterviews'>
+        <span :class="`pi pi-sync ${interviewStore.isLoading ? 'pi-spin' : ''}`" />
+        <span>Refresh</span>
+    </a>
 
-  <div class='job-table'>
-    <a-spin :spinning='interviewStore.getLoading'>
-      <GenericTable
-        :headers="headers"
-        :items="interviewStore.getInterviews"
-      />
-    </a-spin>
-  </div>
+    <div class='job-table'>
+        <a-spin :spinning='interviewStore.getLoading'>
+            <GenericTable :headers="headers" :items="interviewStore.getInterviews">
+                <template #body="{ column, item }">
+                    <template v-if="column == 'actions'">
+                        <a href='#' class="action-btn" @click="interviewModalStore.openModal(item)">
+                            <span class="pi pi-pencil" />
+                            <span>Edit</span>
+                        </a>
+                        <a href='#' class="action-btn">
+                            <span class="pi pi-trash" />
+                            <span>Delete</span>
+                        </a>
+                    </template>
+                </template>
+            </GenericTable>
+        </a-spin>
+    </div>
+    <InterviewModal v-if="interviewModalStore.isOpen" @close="interviewModalStore.closeModal" />
 </template>
 
 <style scoped>
@@ -60,7 +62,7 @@ onMounted(() => {
 }
 
 .job-table {
- overflow: auto; 
+    overflow: auto;
 }
 
 .action-btn {
@@ -83,6 +85,6 @@ onMounted(() => {
 }
 
 .action-btn span:last-child {
-  margin-left: 4px;
+    margin-left: 4px;
 }
 </style>
