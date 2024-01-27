8c94f9f1358ef01453401739dc2577293865e402
Barış Mutlu <baris@mutlu.dev>
2024-01-28 00:31:57 +0300

    BUG: Interview table scrollbar is not positioned at the bottom

--- frontend/src/pages/Interviews.vue
+++ frontend/src/pages/Interviews.vue
@@ -25,15 +25,17 @@ onMounted(() => {
 </script>
 
 <template>
-    <a href='#' class="action-btn" @click.prevent='interviewModalStore.openModal()'>
-        <span class="pi pi-plus" />
-        <span>Create new job</span>
-    </a>
-    <a href='#' class="action-btn" @click.prevent='interviewStore.fetchInterviews'>
-        <span :class="`pi pi-sync ${interviewStore.isLoading ? 'pi-spin' : ''}`" />
-        <span>Refresh</span>
-    </a>
-
+    <div class="job-pane">
+    <div>
+        <a href='#' class="action-btn" @click.prevent='interviewModalStore.openModal()'>
+            <span class="pi pi-plus" />
+            <span>Create new job</span>
+        </a>
+        <a href='#' class="action-btn" @click.prevent='interviewStore.fetchInterviews'>
+            <span :class="`pi pi-sync ${interviewStore.isLoading ? 'pi-spin' : ''}`" />
+            <span>Refresh</span>
+        </a>
+    </div>
     <div class='job-table'>
         <a-spin :spinning='interviewStore.getLoading'>
             <GenericTable :headers="headers" :items="interviewStore.getInterviews">
@@ -54,10 +56,18 @@ onMounted(() => {
             </GenericTable>
         </a-spin>
     </div>
+    </div>
     <InterviewModal v-if="interviewModalStore.isOpen" @close="interviewModalStore.closeModal" />
 </template>
 
 <style scoped>
+
+.job-pane {
+    display: flex;
+    flex-direction: column;
+    height: 100%;
+}
+
 .job-url {
     color: #fff;
     text-decoration: underline;
@@ -65,6 +75,7 @@ onMounted(() => {
 
 .job-table {
     overflow: auto;
+    flex-grow: 1;
 }
 
 .action-btn {
