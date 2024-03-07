ff5295cd5d60c01066ba92cfd4c4242759c13e23
Baris Mutlu <baris@mutlu.dev>
2024-03-08 00:10:18 +0300

    Move theme to separate config file

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -2,32 +2,17 @@
 import { ref } from 'vue'
 import { useAuthStore } from '@/stores/useAuthStore'
 import AuthModal from './components/AuthModal.vue'
+import { theme } from './config/theme'
 const authStore = useAuthStore()
 const isCheckedAuth = ref(false)
 authStore.getUser().finally(() => {
   isCheckedAuth.value = true
 })
-
-const theme = {
-    token: {
-      colorPrimary: "#fff",
-      colorBgBase: "#1b1b1b",
-      colorTextBase: "#fff",
-      colorBorder: "#303030", 
-      colorPrimaryText: "#000",
-      colorTextLightSolid: "#000",
-    }
-  }
-
-
 </script>
 
 <template>
-  <a-config-provider
-    :theme="theme"
-  >
+  <a-config-provider :theme="theme">
   <router-view/>
   <AuthModal v-if='!authStore.isLoggedIn && isCheckedAuth'/>
-
   </a-config-provider>
 </template>
---
+++ frontend/src/config/theme.js
@@ -0,0 +1,10 @@
+export const theme = {
+    token: {
+      colorPrimary: "#fff",
+      colorBgBase: "#1b1b1b",
+      colorTextBase: "#fff",
+      colorBorder: "#303030", 
+      colorPrimaryText: "#000",
+      colorTextLightSolid: "#000",
+    }
+  }
