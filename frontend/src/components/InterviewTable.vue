3d8536e1b31146fa33607edff7aea7f0dc5198fa
Baris Mutlu <baris@mutlu.dev>
2024-03-25 22:58:40 +0300

    Add filtering to status

--- frontend/src/components/InterviewTable.vue
+++ frontend/src/components/InterviewTable.vue
@@ -1,76 +1,183 @@
 <script setup>
-import { onMounted, h } from 'vue'
-import GenericTable from '@/components/GenericTable.vue';
-import { EditOutlined } from '@ant-design/icons-vue';
+import { onMounted, h, ref } from 'vue'
+import GenericTable from '@/components/GenericTable.vue'
+import { EditOutlined, PlusOutlined, ReloadOutlined } from '@ant-design/icons-vue'
 
 import dayjs from 'dayjs'
 import relativeTime from 'dayjs/plugin/relativeTime'
 dayjs.extend(relativeTime)
-
 
 import { useInterviewStore } from '@/stores/InterviewStore'
 import { useInterviewModalStore } from '@/stores/InterviewModalStore'
 const interviewStore = useInterviewStore()
 const interviewModalStore = useInterviewModalStore()
 
+const headers = [
+  { text: 'Company', value: 'company' },
+  { text: 'Title', value: 'title' },
+  { text: 'Deadline', value: 'deadline' },
+  { text: 'Status', value: 'latest-action' },
+  { text: 'Actions', value: 'actions' }
+]
 
-const interviewStatusToEmoji = {
-    wishlist: 'Wishlist⭐️️',
-    applied: 'Applied👏🏻',
-    rejected: 'Rejected❌',
-    accepted: 'Accepted✅'
+const statusOptions = [
+  {
+    name: 'wishlist',
+    text: 'Wishlist⭐️️',
+    baseColor: '#ffeed6',
+    activeColor: '#ff981c'
+  },
+  {
+    name: 'applied',
+    text: 'Applied👏🏻',
+    baseColor: '#f2e3fa',
+    activeColor: '#b764e1'
+  },
+  {
+    name: 'accepted',
+    text: 'Accepted✅',
+    baseColor: '#dff6e4',
+    activeColor: '#48c866'
+  },
+  {
+    name: 'rejected',
+    text: 'Rejected❌',
+    baseColor: '#ffe0de',
+    activeColor: '#ff5047'
+  }
+]
+
+function statusToStyleAdapter(status) {
+  return { '--base-color': status.baseColor, '--active-color': status.activeColor }
 }
 
-const headers = [
-    { text: "Company", value: "company" },
-    { text: "Title", value: "title" },
-    { text: "Deadline", value: "deadline" },
-    { text: "Status", value: "latest-action" },
-    { text: "Actions", value: "actions" }
-];
+const statusFilter = ref(new Set())
 
+function changeStatus(name) {
+  console.log(name)
+  if (statusFilter.value.has(name)) {
+    statusFilter.value.delete(name)
+  } else {
+    statusFilter.value.add(name)
+  }
+}
 
+function filterItems(items) {
+  if (statusFilter.value.size == 0) {
+    return items
+  }
+  return items.filter((item) => {
+    return statusFilter.value.has(item.status)
+  })
+}
 
 onMounted(() => {
-    interviewStore.fetchInterviews()
+  interviewStore.fetchInterviews()
 })
 </script>
 
-
 <template>
-     <div class='job-table'>
-            <a-spin :spinning='interviewStore.getLoading'>
-                <generic-table :headers="headers" :items="interviewStore.getInterviews">
-                    <template #company="{item}">
-                        <img v-if="item.url" :src="`https://www.google.com/s2/favicons?domain=${item.url}`" target="_blank">
-                        <span v-else>🏢</span>
-                        {{ item.company }}
-                    </template>
-                    <template #actions="{item}">
-                        <a-button :icon="h(EditOutlined)" size="small" @click="interviewModalStore.openModal(item)">Edit</a-button>
-                    </template>
-                    <template #deadline="{item}">
-                        {{ dayjs(item.deadline).fromNow() }}
-                    </template>
-                    <template #title="{item}">
-                        <a v-if="item.url" :href="item.url" target="_blank">{{ item.title }} <span class="pi pi-link" /></a>
-                        <span v-else>{{ item.title  }}</span>
-                    </template>
-                    <template #latest-action="{item}">
-                        {{ interviewStatusToEmoji[item.status] }}
-                    </template>
-                </generic-table>
-            </a-spin>
-        </div>
+  <div class="toolbar">
+    <a-button :icon="h(PlusOutlined)" size="small" @click="interviewModalStore.openModal()"
+      >Create new job</a-button
+    >
+    <a-button
+      :icon="h(ReloadOutlined)"
+      size="small"
+      @click="interviewStore.fetchInterviews"
+      :loading="interviewStore.isLoading"
+      >Refresh</a-button
+    >
+    <div class="chips">
+      <div
+        class="chip"
+        :class="{ active: statusFilter.has(status.name) }"
+        :style="statusToStyleAdapter(status)"
+        @click="changeStatus(status.name)"
+        v-for="status in statusOptions"
+        :key="status.name"
+      >
+        {{ status.text }}
+      </div>
+    </div>
+  </div>
+  <div class="job-table">
+    <a-spin :spinning="interviewStore.getLoading">
+      <generic-table :headers="headers" :items="filterItems(interviewStore.getInterviews)">
+        <template #company="{ item }">
+          <img
+            v-if="item.url"
+            :src="`https://www.google.com/s2/favicons?domain=${item.url}`"
+            target="_blank"
+          />
+          <span v-else>🏢</span>
+          {{ item.company }}
+        </template>
+        <template #actions="{ item }">
+          <a-button
+            :icon="h(EditOutlined)"
+            size="small"
+            @click="interviewModalStore.openModal(item)"
+            >Edit</a-button
+          >
+        </template>
+        <template #deadline="{ item }">
+          {{ dayjs(item.deadline).fromNow() }}
+        </template>
+        <template #title="{ item }">
+          <a v-if="item.url" :href="item.url" target="_blank"
+            >{{ item.title }} <span class="pi pi-link"
+          /></a>
+          <span v-else>{{ item.title }}</span>
+        </template>
+        <template #latest-action="{ item }">
+          {{ statusOptions.find((i) => i.name === item.status).text }}
+        </template>
+      </generic-table>
+    </a-spin>
+  </div>
 </template>
 
 <style scoped>
 a {
-    color: #fff;
+  color: #fff;
 }
 
 .job-table {
-    overflow: auto;
-    flex-grow: 1;
+  overflow: auto;
+  flex-grow: 1;
+}
+
+.toolbar {
+  display: flex;
+  align-items: center;
+  flex-direction: row;
+  gap: 8px;
+  flex-wrap: wrap;
+}
+
+.chips {
+  display: flex;
+  gap: 8px;
+  flex-wrap: nowrap;
+  overflow: auto;
+}
+
+.chip {
+  border: 1px solid var(--active-color);
+  color: var(--active-color);
+  background-color: var(--base-color);
+  border-radius: 40px;
+  text-align: center;
+  font-size: 14px;
+  padding: 4px 10px;
+  user-select: none;
+  cursor: pointer;
+  white-space: nowrap;
+}
+
+.chip.active {
+  background-color: var(--active-color);
+  color: white;
 }
 </style>
--- frontend/src/pages/Interviews.vue
+++ frontend/src/pages/Interviews.vue
@@ -1,46 +1,37 @@
 <script setup>
-import { h } from 'vue'
-import { PlusOutlined, ReloadOutlined } from '@ant-design/icons-vue';
-import InterviewModal from '@/components/InterviewModal.vue';
-import interviewTable from '@/components/InterviewTable.vue';
+import InterviewModal from '@/components/InterviewModal.vue'
+import interviewTable from '@/components/InterviewTable.vue'
 import { useInterviewModalStore } from '@/stores/InterviewModalStore'
-import { useInterviewStore } from '@/stores/InterviewStore'
 const interviewModalStore = useInterviewModalStore()
-const interviewStore = useInterviewStore()
 </script>
 
 <template>
-    <div class="dashboard">
-            <div class="job-pane">
-                <a-space>
-                    <a-button :icon="h(PlusOutlined)" size="small" @click="interviewModalStore.openModal()">Create new job</a-button>
-                    <a-button :icon="h(ReloadOutlined)" size="small" @click="interviewStore.fetchInterviews" :loading="interviewStore.isLoading">Refresh</a-button>
-                </a-space>
-                <interview-table />
-            </div>
+  <div class="dashboard">
+    <div class="job-pane">
+      <interview-table />
     </div>
-    
-    <InterviewModal v-if="interviewModalStore.isOpen" @close="interviewModalStore.closeModal" />
+  </div>
+
+  <InterviewModal v-if="interviewModalStore.isOpen" @close="interviewModalStore.closeModal" />
 </template>
 
 <style scoped>
-
 .dashboard {
-    width: 100%;
-    height: 100%;
-    display: grid;
-    grid-template-columns: repeat(2, 1fr);
-    grid-template-rows: 200px 1fr;
-    grid-template-areas: 
-        "interviews interviews"
-        "interviews interviews";
+  width: 100%;
+  height: 100%;
+  display: grid;
+  grid-template-columns: repeat(2, 1fr);
+  grid-template-rows: 200px 1fr;
+  grid-template-areas:
+    'interviews interviews'
+    'interviews interviews';
 }
 
 .job-pane {
-    grid-area: interviews;
-    display: flex;
-    flex-direction: column;
-    height: 100%;
-    overflow: auto;
+  grid-area: interviews;
+  display: flex;
+  flex-direction: column;
+  height: 100%;
+  overflow: auto;
 }
 </style>
