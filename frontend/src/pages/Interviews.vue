1a01bf445af5527f1e2f57f5e84d396778b426a5
Baris Mutlu <baris@mutlu.dev>
2024-03-25 14:11:10 +0300

    Move Interview Table to another component and make display grid

--- frontend/src/pages/Interviews.vue
+++ frontend/src/pages/Interviews.vue
@@ -1,86 +1,46 @@
 <script setup>
-import { onMounted, h } from 'vue'
-import { PlusOutlined, ReloadOutlined, EditOutlined } from '@ant-design/icons-vue';
+import { h } from 'vue'
+import { PlusOutlined, ReloadOutlined } from '@ant-design/icons-vue';
 import InterviewModal from '@/components/InterviewModal.vue';
-import GenericTable from '@/components/GenericTable.vue';
-
-import dayjs from 'dayjs'
-import relativeTime from 'dayjs/plugin/relativeTime'
-dayjs.extend(relativeTime)
-
-import { useInterviewStore } from '@/stores/InterviewStore'
+import interviewTable from '@/components/InterviewTable.vue';
 import { useInterviewModalStore } from '@/stores/InterviewModalStore'
-const interviewStore = useInterviewStore()
+import { useInterviewStore } from '@/stores/InterviewStore'
 const interviewModalStore = useInterviewModalStore()
-
-const interviewStatusToEmoji = {
-    wishlist: 'Wishlist⭐️️',
-    applied: 'Applied👏🏻',
-    rejected: 'Rejected❌',
-    accepted: 'Accepted✅'
-}
-
-const headers = [
-    { text: "Company", value: "company" },
-    { text: "Title", value: "title" },
-    { text: "Deadline", value: "deadline" },
-    { text: "Latest Action", value: "latest-action" },
-    { text: "Actions", value: "actions" }
-];
-
-onMounted(() => {
-    interviewStore.fetchInterviews()
-})
+const interviewStore = useInterviewStore()
 </script>
 
 <template>
-    <div class="job-pane">
-        <a-space>
-            <a-button :icon="h(PlusOutlined)" size="small" @click="interviewModalStore.openModal()">Create new job</a-button>
-            <a-button :icon="h(ReloadOutlined)" size="small" @click="interviewStore.fetchInterviews" :loading="interviewStore.isLoading">Refresh</a-button>
-        </a-space>
-        <div class='job-table'>
-            <a-spin :spinning='interviewStore.getLoading'>
-                <GenericTable :headers="headers" :items="interviewStore.getInterviews">
-                    <template #company="{item}">
-                        <img v-if="item.url" :src="`https://www.google.com/s2/favicons?domain=${item.url}`" target="_blank">
-                        <span v-else>🏢</span>
-                        {{ item.company }}
-                    </template>
-                    <template #actions="{item}">
-                        <a-button :icon="h(EditOutlined)" size="small" @click="interviewModalStore.openModal(item)">Edit</a-button>
-                    </template>
-                    <template #deadline="{item}">
-                        {{ dayjs(item.deadline).fromNow() }}
-                    </template>
-                    <template #title="{item}">
-                        <a v-if="item.url" :href="item.url" target="_blank">{{ item.title }} <span class="pi pi-link" /></a>
-                        <span v-else>{{ item.title  }}</span>
-                    </template>
-                    <template #latest-action="{item}">
-                        {{ interviewStatusToEmoji[item.status] }}
-                    </template>
-                </GenericTable>
-            </a-spin>
-        </div>
+    <div class="dashboard">
+            <div class="job-pane">
+                <a-space>
+                    <a-button :icon="h(PlusOutlined)" size="small" @click="interviewModalStore.openModal()">Create new job</a-button>
+                    <a-button :icon="h(ReloadOutlined)" size="small" @click="interviewStore.fetchInterviews" :loading="interviewStore.isLoading">Refresh</a-button>
+                </a-space>
+                <interview-table />
+            </div>
     </div>
+    
     <InterviewModal v-if="interviewModalStore.isOpen" @close="interviewModalStore.closeModal" />
 </template>
 
 <style scoped>
 
+.dashboard {
+    width: 100%;
+    height: 100%;
+    display: grid;
+    grid-template-columns: repeat(2, 1fr);
+    grid-template-rows: 200px 1fr;
+    grid-template-areas: 
+        "interviews interviews"
+        "interviews interviews";
+}
+
 .job-pane {
+    grid-area: interviews;
     display: flex;
     flex-direction: column;
     height: 100%;
-}
-
-a {
-    color: #fff;
-}
-
-.job-table {
     overflow: auto;
-    flex-grow: 1;
 }
 </style>
