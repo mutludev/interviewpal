0be0f4be893baad8575c50068d2307d9710fb4ab
Baris Mutlu <baris@mutlu.dev>
2024-03-25 14:10:39 +0300

    Create new component for Interview Table

---
+++ frontend/src/components/InterviewTable.vue
@@ -0,0 +1,76 @@
+<script setup>
+import { onMounted, h } from 'vue'
+import GenericTable from '@/components/GenericTable.vue';
+import { EditOutlined } from '@ant-design/icons-vue';
+
+import dayjs from 'dayjs'
+import relativeTime from 'dayjs/plugin/relativeTime'
+dayjs.extend(relativeTime)
+
+
+import { useInterviewStore } from '@/stores/InterviewStore'
+import { useInterviewModalStore } from '@/stores/InterviewModalStore'
+const interviewStore = useInterviewStore()
+const interviewModalStore = useInterviewModalStore()
+
+
+const interviewStatusToEmoji = {
+    wishlist: 'Wishlist⭐️️',
+    applied: 'Applied👏🏻',
+    rejected: 'Rejected❌',
+    accepted: 'Accepted✅'
+}
+
+const headers = [
+    { text: "Company", value: "company" },
+    { text: "Title", value: "title" },
+    { text: "Deadline", value: "deadline" },
+    { text: "Status", value: "latest-action" },
+    { text: "Actions", value: "actions" }
+];
+
+
+
+onMounted(() => {
+    interviewStore.fetchInterviews()
+})
+</script>
+
+
+<template>
+     <div class='job-table'>
+            <a-spin :spinning='interviewStore.getLoading'>
+                <generic-table :headers="headers" :items="interviewStore.getInterviews">
+                    <template #company="{item}">
+                        <img v-if="item.url" :src="`https://www.google.com/s2/favicons?domain=${item.url}`" target="_blank">
+                        <span v-else>🏢</span>
+                        {{ item.company }}
+                    </template>
+                    <template #actions="{item}">
+                        <a-button :icon="h(EditOutlined)" size="small" @click="interviewModalStore.openModal(item)">Edit</a-button>
+                    </template>
+                    <template #deadline="{item}">
+                        {{ dayjs(item.deadline).fromNow() }}
+                    </template>
+                    <template #title="{item}">
+                        <a v-if="item.url" :href="item.url" target="_blank">{{ item.title }} <span class="pi pi-link" /></a>
+                        <span v-else>{{ item.title  }}</span>
+                    </template>
+                    <template #latest-action="{item}">
+                        {{ interviewStatusToEmoji[item.status] }}
+                    </template>
+                </generic-table>
+            </a-spin>
+        </div>
+</template>
+
+<style scoped>
+a {
+    color: #fff;
+}
+
+.job-table {
+    overflow: auto;
+    flex-grow: 1;
+}
+</style>
